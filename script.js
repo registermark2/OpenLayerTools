1f69cfa76244a00b89de706448a981763d667b7b
Andy <registermark3@gmail.com>
2021-03-23 19:09:31 +0800

    just a little change.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var loc = [];
 
 $.getJSON(
@@ -41,20 +39,12 @@ var omsMap = new ol.layer.Tile({
     source: new ol.source.OSM()
 });
 
-
-
-
-
-
-
-
 var wmtsMap = new ol.layer.Tile({
     source: new ol.source.XYZ({
     url:
         'https://wmts.nlsc.gov.tw/wmts/EMAP5/default/EPSG:3857/{z}/{y}/{x}.png'
     })
 });
-
 
 var map = new ol.Map({
     layers: [
@@ -64,7 +54,8 @@ var map = new ol.Map({
     target: document.getElementById('map'),
     view: new ol.View({
         projection: 'EPSG:3857',
-        center: ol.proj.fromLonLat([120.950472, 23.814073]),
-        zoom: 8
+        center: ol.proj.fromLonLat([120.846642, 23.488793]),
+        zoom: 8.3
     })
 });
+
