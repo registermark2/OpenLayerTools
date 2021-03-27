a5e0743fb5f29ac6d85fed5fa4aeb67115db0a03
Andy <registermark3@gmail.com>
2021-03-27 23:21:36 +0800

    add new weather station data.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var rainfallStationAPI_URL = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-326DAE79-B70E-4DD3-BC36-07B077E82CAB&elementName=NOW&parameterName=CITY,TOWN,ATTRIBUTE";
-var weatherStationAPI_URL = "";
+var weatherStationAPI_URL = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-326DAE79-B70E-4DD3-BC36-07B077E82CAB&elementName=H_24R&parameterName%EF%BC%8C=CITY,TOWN";
+var rainfallStationAPI_Flag = 0;
+var weatherStationAPI_Flag = 0;
 
 
 var map;
-var clusters;
-var clusterSource;
+var rainfallStationClusters; // for remove rainfall station layers
+var weatherStationCluster; // for remove weather station layers
+var rainfallStationClusterSource;
+var weatherStationClusterSource;
 
 readRainfallAPI = function(){
     $.getJSON(
         rainfallStationAPI_URL,
         function (res) {
+            // console.log(res.records.location[0].parameter[1].parameterValue);
+
             var distance = document.getElementById('distance');
 
             var count = Object.keys(res.records.location).length;
@@ -23,6 +29,7 @@ readRainfallAPI = function(){
                                             id: res.records.location[i].stationId,
                                             value: res.records.location[i].weatherElement[0].elementValue,
                                             time: res.records.location[i].time.obsTime,
+                                            district: res.records.location[i].parameter[1].parameterValue,
                                         });
             }
 
@@ -30,14 +37,86 @@ readRainfallAPI = function(){
                 features: features
             });
             console.log
-            clusterSource = new ol.source.Cluster({
+            rainfallStationClusterSource = new ol.source.Cluster({
                 distance: parseInt(distance.value, 10),
                 source: source
             });
 
             var styleCache = {};
-            clusters = new ol.layer.Vector({
-                source: clusterSource,
+            rainfallStationClusters = new ol.layer.Vector({
+                source: rainfallStationClusterSource,
+                style: function (feature) {
+                    var size = feature.get('features').length;
+                    var style = styleCache[size];
+                    if (!style) {
+                        style = new ol.style.Style({
+                            image: new ol.style.Circle({
+                                radius: 15,
+                                stroke: new ol.style.Stroke({
+                                    color: '#FAA428'
+                                }),
+                                fill: new ol.style.Fill({
+                                    color: '#FFD162'
+                                })
+                            }),
+                            text: new ol.style.Text({
+                                text: size.toString(),
+                                fill: new ol.style.Fill({
+                                    color: '#000'
+                                })
+                            })
+                        });
+                        styleCache[size] = style;
+                    }
+                    return style;
+                }
+            });
+            
+            distance.addEventListener('input', function () {
+                rainfallStationClusterSource.setDistance(parseInt(distance.value, 10));
+                
+            });
+            map.addLayer(rainfallStationClusters);
+            
+        }
+    )
+};
+
+
+
+readWeatherStationAPI = function(){
+    $.getJSON(
+        weatherStationAPI_URL,
+        function (res) {
+            var distance = document.getElementById('distance');
+            console.log(res);
+            var count = Object.keys(res.records.location).length;
+            var features = new Array(count);
+            for (var i = 0; i < count; ++i) {
+                // console.log(res.records.location[i]);
+                // console.log(res.records.location[i].weatherElement[0].elementValue);
+                var coordinates = ol.proj.fromLonLat([res.records.location[i].lon, res.records.location[i].lat]);
+                features[i] = new ol.Feature({
+                                            geometry: new ol.geom.Point(coordinates),
+                                            id: res.records.location[i].stationId,
+                                            // value: res.records.location[i].weatherElement[0].elementValue,
+                                            time: res.records.location[i].time.obsTime,
+                                            district: res.records.location[i].parameter[1].parameterValue,
+                                        });
+            }
+
+            var source = new ol.source.Vector({
+                features: features
+            });
+            console.log
+            weatherStationClusterSource = new ol.source.Cluster({
+                distance: parseInt(distance.value, 10),
+                source: source
+            });
+
+            var styleCache = {};
+            weatherStationCluster = new ol.layer.Vector({
+                source: weatherStationClusterSource,
                 style: function (feature) {
                     var size = feature.get('features').length;
                     var style = styleCache[size];
@@ -66,34 +145,35 @@ readRainfallAPI = function(){
             });
             
             distance.addEventListener('input', function () {
-                clusterSource.setDistance(parseInt(distance.value, 10));
+                weatherStationClusterSource.setDistance(parseInt(distance.value, 10));
                 
             });
-            map.addLayer(clusters);
-            
+            map.addLayer(weatherStationCluster);
         }
     )
 };
-// readWeatherStationAPI = function(){
-//     $.getJSON(
-//         weatherStationAPI_URL,
-//         function (res) {
 
-//         }
-//     )
-// };
 
 
 
 
 // select checkbox show data
-$("#checkBox").change(function() {
+$("#checkBoxRainfallStation").change(function() {
     if(this.checked) {
         readRainfallAPI();
     }else{
-        map.removeLayer(clusters);
+        map.removeLayer(rainfallStationClusters);
     }
 });
+
+$("#checkBoxWeatherStation").change(function() {
+    if(this.checked) {
+        readWeatherStationAPI();
+    }else{
+        map.removeLayer(weatherStationCluster);
+    }
+});
+
 
 
 
@@ -150,25 +230,48 @@ map.addOverlay(popup);
 // decide popup show data and disappear
 map.on('pointermove', function(evt){
     var featureClusterNumber;
+    var featureCLusterDistrict;
     var feature = map.forEachFeatureAtPixel(evt.pixel,
         function(feature){
             var features = feature.get('features');
+            // console.log()
             featureClusterNumber = feature.get('features').length;
-            return features[0]; //get cluster value level
+            return features;
     });
     if(feature){
-        // console.log(feature);
-        var coordinates = feature.getGeometry().getCoordinates();
+        var coordinates = feature[0].getGeometry().getCoordinates();
         popup.setPosition(coordinates);
         document.getElementById("popup").classList.add("ol-popup");
 
-        if(featureClusterNumber<2){
-            content.innerHTML = '<div>'+"站點: "+feature.get('id')+'</div>'+
-                                '<div>'+"雨量: "+feature.get('value')+" mm"+'</div>'+ 
-                                '<div>'+"時間: "+feature.get('time')+'</div>';
+        if(featureClusterNumber==1){// decide range of district 
+            content.innerHTML = '<div>'+"站點: "+feature[0].get('id')+"</div>"+
+                                '<div>'+"時間: "+feature[0].get('time')+'</div>'+
+                                '<div>'+"雨量: "+feature[0].get('value')+" mm"+'</div>'+ 
+                                '<div>'+"地區: "+feature[0].get('district')+'</div>';
+
             overlay.setPosition(coordinates);
         }else{
-            content.innerHTML = '<div>'+featureClusterNumber+'個測站'+'</div>';
+            var districtString = feature[0].get('district')+",";
+            if (featureClusterNumber>5){
+                for(var i =1;i<5;i++){
+                    if(i==4){
+                        districtString+=feature[i].get('district');
+                    }
+                    if (i<4){
+                        districtString+=feature[i].get('district')+",";
+                    }
+                }
+            }else{
+                for(var i =1;i<featureClusterNumber;i++){
+                    if(i==(featureClusterNumber-1)){
+                        districtString+=feature[i].get('district');
+                    }
+                    if (i<featureClusterNumber-1){
+                        districtString+=feature[i].get('district')+",";
+                    }
+                }
+            }
+            content.innerHTML = districtString+"....";
             overlay.setPosition(coordinates);
         }
     } else {
