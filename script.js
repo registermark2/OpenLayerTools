3e6b347e54fc9211ef363be6ec6a0e745d63ce30
DESKTOP-31AFM8E\ANDY <registermark3@gmail.com>
2021-03-29 17:16:24 +0800

    js remove map on function.

--- script.js
+++ script.js
@@ -265,7 +265,6 @@ $("#checkBoxRainfallStation").change(function() {
     if(this.checked) {
         readRainfallAPI(rainfallStationAPI_URL, rainfallStyle);
         test();
-        
     }else{
         map.removeLayer(rainfallStationClusters);
         
@@ -343,65 +342,65 @@ map.addOverlay(popup);
 // decide popup show data and disappear
 
 
-test = function(){    
-    map.on('pointermove', function(evt){
-        var featureClusterNumber;
-        var featureCLusterDistrict;
-        var feature = map.forEachFeatureAtPixel(evt.pixel,
-            function(feature){
-                var features = feature.get('features');
-                // console.log(feature)
-                featureClusterNumber = feature.get('features').length;
-                return features;
-        });
-        if(feature){
-            var coordinates = feature[0].getGeometry().getCoordinates();
-            popup.setPosition(coordinates);
-            document.getElementById("popup").classList.add("ol-popup");
+// test = function(){    
+map.on('pointermove', function(evt){
+    var featureClusterNumber;
+    var featureCLusterDistrict;
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+        function(feature){
+            var features = feature.get('features');
+            // console.log(feature)
+            featureClusterNumber = feature.get('features').length;
+            return features;
+    });
+    if(feature){
+        var coordinates = feature[0].getGeometry().getCoordinates();
+        popup.setPosition(coordinates);
+        document.getElementById("popup").classList.add("ol-popup");
 
-            if(featureClusterNumber==1){// decide range of district 
-                content.innerHTML = '<div>'+"站點: "+feature[0].get('id')+"</div>"+
-                                    '<div>'+"時間: "+feature[0].get('time')+'</div>'+
-                                    '<div>'+"雨量: "+feature[0].get('value')+" mm"+'</div>'+ 
-                                    '<div>'+"地區: "+feature[0].get('district')+'</div>';
+        if(featureClusterNumber==1){// decide range of district 
+            content.innerHTML = '<div>'+"站點: "+feature[0].get('id')+"</div>"+
+                                '<div>'+"時間: "+feature[0].get('time')+'</div>'+
+                                '<div>'+"雨量: "+feature[0].get('value')+" mm"+'</div>'+ 
+                                '<div>'+"地區: "+feature[0].get('district')+'</div>';
 
-                overlay.setPosition(coordinates);
-            }else{
-                var districtString = feature[0].get('district')+",";
-                if (featureClusterNumber>5){
-                    for(var i =1;i<5;i++){
-                        if(i==4){
-                            districtString+=feature[i].get('district');
-                        }
-                        if (i<4){
-                            districtString+=feature[i].get('district')+",";
-                        }
+            overlay.setPosition(coordinates);
+        }else{
+            var districtString = feature[0].get('district')+",";
+            if (featureClusterNumber>5){
+                for(var i =1;i<5;i++){
+                    if(i==4){
+                        districtString+=feature[i].get('district');
                     }
-                }else{
-                    for(var i =1;i<featureClusterNumber;i++){
-                        if(i==(featureClusterNumber-1)){
-                            districtString+=feature[i].get('district');
-                        }
-                        if (i<featureClusterNumber-1){
-                            districtString+=feature[i].get('district')+",";
-                        }
+                    if (i<4){
+                        districtString+=feature[i].get('district')+",";
                     }
                 }
-                content.innerHTML = districtString+"....";
-                overlay.setPosition(coordinates);
+            }else{
+                for(var i =1;i<featureClusterNumber;i++){
+                    if(i==(featureClusterNumber-1)){
+                        districtString+=feature[i].get('district');
+                    }
+                    if (i<featureClusterNumber-1){
+                        districtString+=feature[i].get('district')+",";
+                    }
+                }
             }
-        } else {
-            content.innerHTML ="";
-            document.getElementById("popup").classList.remove("ol-popup");
-            overlay.setPosition(undefined);
+            content.innerHTML = districtString+"....";
+            overlay.setPosition(coordinates);
         }
-        
-    });
+    } else {
+        content.innerHTML ="";
+        document.getElementById("popup").classList.remove("ol-popup");
+        overlay.setPosition(undefined);
+    }
+    
+});
 
-    // change mouse cursor when over marker
-    map.on('pointermove', function(e) {
-        var pixel = map.getEventPixel(e.originalEvent);
-        var hit = map.hasFeatureAtPixel(pixel);
-        map.getTarget().style.cursor = hit ? 'pointer' : '';
-    });
-}
+// change mouse cursor when over marker
+map.on('pointermove', function(e) {
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
+// }
